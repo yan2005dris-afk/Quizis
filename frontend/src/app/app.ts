2ed66e49d9908eacb88387f3adb8d3139a1fc0be
SaidJuniorProgrammer <saidpintotamayo3731@upse.edu.ec>
2026-05-23 18:19:37 -0500

    Refactor App component by removing unused imports
    Removed unused SocketService and OnInit lifecycle hook.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,21 +1,12 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SocketService } from './core/services/socket.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet], 
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
+export class App {
   protected readonly title = signal('frontend');
-
-  
-  constructor(private socketService: SocketService) {}
-
-  
-  ngOnInit() {
-    this.socketService.unirseASala('UPSE-123', 'Said');
-  }
 }
