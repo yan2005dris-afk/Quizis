db967980ac5264b3776c05b5ff13fbf44f2ed00e
CarlosFernandoPatinoGarcia <carlos.patinogarcia9447@upse.edu.ec>
2026-05-24 18:00:35 -0500

    test (comodin-llamada.service): agregar spec al comodin de llamada

--- backend/src/comodines/comodin-llamada/comodin-llamada.service.spec.ts
+++ backend/src/comodines/comodin-llamada/comodin-llamada.service.spec.ts
@@ -1,15 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ComodinLlamadaService } from './comodin-llamada.service';
+import { PrismaService } from 'src/infrastructure/database/prisma.service';
 
 describe('ComodinLlamadaService', () => {
   let service: ComodinLlamadaService;
+  let prisma: PrismaService;
+
+  /**
+   * Ultimos cambios hechos:
+   *
+   * - Uso de transaction
+   * - Validaciones adicionales
+   * - Refactorizacion de codigo
+   *
+   * @author Carlos Patiño
+   * @date 2026-05-24
+   */
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ComodinLlamadaService],
+      providers: [
+        ComodinLlamadaService,
+        {
+          provide: PrismaService,
+          useValue: {
+            extendedClient: {
+              rondas: {
+                findFirst: jest.fn(),
+              },
+              participantes: {
+                findMany: jest.fn(),
+              },
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ComodinLlamadaService>(ComodinLlamadaService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
