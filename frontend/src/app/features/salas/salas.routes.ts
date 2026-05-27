df7f7a7662b79f49d2e3d03ade07edc8f4884fb0
jean920cedeno <jean.cedenoandrade5763@upse.edu.ec>
2026-05-26 22:31:52 -0500

    feat: pantalla podio de jugadores y generacion de reportes
    - Agrega componente analytics
    - Agrega componente game-over
    - Agrega servicio de reportes
    - Actualiza rutas de room y salas
    - Avisar disconformidades y correcciones a realizar si viene al caso

--- frontend/src/app/features/salas/salas.routes.ts
+++ frontend/src/app/features/salas/salas.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 
+
 export const SALAS_ROUTES: Routes = [
   {
     path: '',
     loadComponent: () =>
       import('./pages/salas-index/salas-index.component').then((m) => m.SalasIndexComponent),
   },
+  
+  {
+    path: ':id/game-over',
+    loadComponent: () =>
+      import('../room/pages/game-over/game-over.component').then((m) => m.GameOverComponent),
+  },
+  {
+    path: ':id/analiticas',
+    loadComponent: () =>
+      import('../room/pages/analytics/analytics.component').then((m) => m.AnalyticsComponent),
+  },  
 ];
