67f9175ec5ba8d5ad8de18ab7254cd350fc5cd26
Ali-Rr26 <150749255+Ali-Rr26@users.noreply.github.com>
2026-05-27 18:19:45 -0500

    add dockerignore and remove unused docker env config

--- backend/prisma/migrations/20260525193120_migrate_25052026/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `is_online` on the `participantes` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "participantes" DROP COLUMN "is_online";
