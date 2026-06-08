b1cc624dbb98391c018571706a36bc9898036430
Yandris Rivera Torres <yan.2005dris@gmail.com>
2026-06-07 19:32:34 -0500

    Delete migration.sql

--- backend/prisma/migrations/20260608000728_add_participante_id_to_respuestas_ronda/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "respuestas_ronda" ADD COLUMN     "participante_id" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "respuestas_ronda" ADD CONSTRAINT "respuestas_ronda_participante_id_fkey" FOREIGN KEY ("participante_id") REFERENCES "participantes"("participante_id") ON DELETE SET NULL ON UPDATE CASCADE;
